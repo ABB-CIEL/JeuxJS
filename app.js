3959ccc0dfa449d16c0c21cff4cd5688ee28fd28
thierry abbey <habbey480@gmail.com>
2025-09-09 11:58:09 +0200

    #commits4Application texte chat

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ exp.use(express.static(__dirname + '/www'));
 //   Paramétrer la réponse à une requête GET avec une url sur la racine et sans nom de fichier : 
 exp.get('/', function (req, res) {
     console.log('Réponse à un client');
-    res.sendFile(__dirname + '/www/index.html');
+    res.sendFile(__dirname + '/www/textchat.html');
 });
 
 //  Ajouter le traitement en cas d’erreur sur le serveur
@@ -31,26 +31,7 @@ exp.use(function (err, req, res, next) {
 
 /*  *************** serveur WebSocket express *********************   */
 // 
-var expressWs = require('express-ws')(exp);
 
-// Connexion des clients à la WebSocket /echo et evenements associés 
-exp.ws('/echo', function (ws, req) {
-
-    console.log('Connection WebSocket %s sur le port %s',
-        req.connection.remoteAddress, req.connection.remotePort);
-
-    ws.on('message', function (message) {
-        console.log('De %s %s, message :%s', req.connection.remoteAddress,
-            req.connection.remotePort, message);
-        ws.send(message);
-    });
-
-    ws.on('close', function (reasonCode, description) {
-        console.log('Deconnexion WebSocket %s sur le port %s',
-            req.connection.remoteAddress, req.connection.remotePort);
-    });
-
-}); 
 /*  *************** serveur WebSocket express *********************   */
 // 
 var expressWs = require('express-ws')(exp);
@@ -62,7 +43,10 @@ exp.ws('/echo', function (ws, req) {
     ws.on('message', function (message) {
         console.log('De %s %s, message :%s', req.connection.remoteAddress, req.connection.remotePort, message);
         ws.send(message); // Echo du message
+        message = ws._socket._peername.address + ws._socket._peername.port + ' : ' + message;
+        aWss.broadcast(message);
     });
+ 
 
     ws.on('close', function () {
         console.log('Deconnexion WebSocket %s sur le port %s', req.connection.remoteAddress, req.connection.remotePort);
@@ -72,3 +56,22 @@ var portServ = 80;
 exp.listen(portServ, function () {
     console.log('Serveur en ecoute');
 });
+/*  ****************** Broadcast clients WebSocket  **************   */
+var aWss = expressWs.getWss('/echo'); 
+var WebSocket = require('ws');
+
+aWss.broadcast = function broadcast(data) {
+    console.log("Broadcast aux clients navigateur : %s", data);
+    aWss.clients.forEach(function each(client) {
+        if (client.readyState == WebSocket.OPEN) {
+            client.send(data, function ack(error) {
+                console.log("    -  %s-%s", client._socket.remoteAddress,
+                    client._socket.remotePort);
+                if (error) {
+                    console.log('ERREUR websocket broadcast : %s', error.toString());
+                }
+            });
+        }
+    });
+}; 
+
